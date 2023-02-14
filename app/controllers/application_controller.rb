2817de969da0d0e67acf21c9e8fcacb6d876c195
Mizuki <MIzuki@Maruo-MBPro.local>
2023-02-14 14:58:44 +0900

    Add:　ストロングパラメータを追加
    プロフィール画面実装のため、ストロングパラメータを追加。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_account_update_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def configure_account_update_parameters
+   devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :profile, :image])
+  end
+
 end
