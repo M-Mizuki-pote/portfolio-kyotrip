93cae7479e0ac52b2c3736bf4e16b0d192f31d33
Mizuki <MIzuki@Maruo-MBPro.local>
2023-02-14 15:35:37 +0900

    Add:　ルーティングを追加
    ログアウト用のルーティングを追加。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'users#index'
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
+
   devise_for :users
   resources :users
 end
